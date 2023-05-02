fe75fcc39a35ac07ef8b027fe54185565341c400
David Rans <dave.g.rans@gmail.com>
2023-05-02 02:23:16 -0600

    Delete commented code

--- api/index.php
+++ api/index.php
@@ -7,30 +7,6 @@ $db = [
    'phone' => '+1-805-555-5555',
 ];
 
-/* $inputClasses = ' mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 '; */
-/* $editForm = ' */
-/* <form class="grid grid-cols-1 gap-6 p-7 w-80 bg-white rounded-lg text-gray-600"> */
-/*     <h2 class="text-2xl">Edit Card</h2> */
-/*     <label class="block"> */
-/*         <span class="text-gray-700">Full name</span> */
-/*         <input type="text" name="name" value="${db.name}" class="${inputClasses}" /> */
-/*     </label> */
-/*     <label class="block"> */
-/*         <span class="text-gray-700">Title</span> */
-/*         <input type="text" name="title" value="${db.title}" class="${inputClasses}" /> */
-/*     </label> */
-/*     <label class="block"> */
-/*         <span class="text-gray-700">Email</span> */
-/*         <input type="email" name="email" value="${db.email}" class="${inputClasses}" /> */
-/*     </label> */
-/*     <label class="block"> */
-/*         <span class="text-gray-700">Phone</span> */
-/*         <input type="tel" name="phone" value="${db.phone}"  class="${inputClasses}" /> */
-/*     </label> */
-/*     <button type="submit">Save</button> */
-/* </form> */
-/* '; */
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -39,7 +15,6 @@ $db = [
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>David Rans</title>
-    <!-- <script src="https://cdn.tailwindcss.com?plugins=forms"></script> -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   </head>
